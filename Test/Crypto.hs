fdfb1b9b6fcc367235f75b79598a7f0228146f15
Thomas M. DuBuisson <tommd@galois.com>
2013-05-07 10:55:57 -0700

    retab

--- Test/Crypto.hs
+++ Test/Crypto.hs
@@ -20,16 +20,16 @@
    algorithms.
 -}
 module Test.Crypto
-	(
-	-- * Block Cipher KATs
-	  makeBlockCipherPropTests
-	-- * Hash property tests
-	, makeHashPropTests
-	-- * Utils
-	, hexStringToBS
-	-- * Re-exported
+        (
+        -- * Block Cipher KATs
+          makeBlockCipherPropTests
+        -- * Hash property tests
+        , makeHashPropTests
+        -- * Utils
+        , hexStringToBS
+        -- * Re-exported
         , defaultMain
-	) where
+        ) where
 
 import Test.QuickCheck
 import Test.ParseNistKATs
@@ -67,20 +67,20 @@ instance Arbitrary L.ByteString where
 prop_LazyStrictEqual :: Hash c d => d -> L.ByteString -> Bool
 prop_LazyStrictEqual d lps =
     let strict   = B.concat $ L.toChunks lps
-	f  = hashFunc d
-	f' = hashFunc' d
+        f  = hashFunc d
+        f' = hashFunc' d
     in f lps == f' strict
 
 -- |Verify the Serialize and Binary instances result
 -- in bytestrings of the correct length for a given digest
 prop_DigestLen :: Hash c d => d -> L.ByteString -> Bool
 prop_DigestLen d lps =
-	fromIntegral o == L.length h && o == B.length h'
+        fromIntegral o == L.length h && o == B.length h'
   where f = hashFunc d
-	f' = hashFunc' d
-	h = L.fromChunks [Ser.encode $ f lps]
-	h' = Ser.encode . f' . B.concat . L.toChunks $ lps
-	o = (outputLength `for` d) `div` 8
+        f' = hashFunc' d
+        h = L.fromChunks [Ser.encode $ f lps]
+        h' = Ser.encode . f' . B.concat . L.toChunks $ lps
+        o = (outputLength `for` d) `div` 8
 
 -- |Verify the Serilize and Binary (decode . encode = id)
 prop_GetPutHash :: Hash c d => d -> L.ByteString -> Bool
@@ -117,30 +117,30 @@ prop_OutputLengthIsByteAligned d = blockLength .::. d `rem` 8 == 0
 --    * The digest (output) length is byte aligned (also needed by 'crypto-api')
 makeHashPropTests :: Hash c d => d -> Test
 makeHashPropTests d =
-	testGroup "Cryptographic Digest Property Tests"
-	[ testProperty "LazyStrictEqual" (prop_LazyStrictEqual d)
-	, testProperty "DigestLen" (prop_DigestLen d)
-	, testProperty "GetPutHash" (prop_GetPutHash d)
-	, testProperty "BlockLengthIsByteAligned"(prop_BlockLengthIsByteAligned d)
-	, testProperty "OutputLengthIsByteAligned"  (prop_OutputLengthIsByteAligned d)
-	]
+        testGroup "Cryptographic Digest Property Tests"
+        [ testProperty "LazyStrictEqual" (prop_LazyStrictEqual d)
+        , testProperty "DigestLen" (prop_DigestLen d)
+        , testProperty "GetPutHash" (prop_GetPutHash d)
+        , testProperty "BlockLengthIsByteAligned"(prop_BlockLengthIsByteAligned d)
+        , testProperty "OutputLengthIsByteAligned"  (prop_OutputLengthIsByteAligned d)
+        ]
 
 -- |some generic blockcipher tests
 
 goodKey :: BlockCipher k => k -> B.ByteString -> Bool
 goodKey k bs =
-	case (getKey k bs `asTypeOf` Just k) of
-		Nothing -> False
-		Just _  -> True
+        case (getKey k bs `asTypeOf` Just k) of
+                Nothing -> False
+                Just _  -> True
 
 bKey k bs = let Just k' = (getKey k bs `asTypeOf` Just k) in k'
 
 -- Pad out (or trim) material to correct length (for testing only!)
 getKey :: BlockCipher k => k -> B.ByteString -> Maybe k
 getKey k bs =
-	let l  = (keyLength `for` k) `div` 8
-	    b' = B.take l (B.concat $ replicate l (B.append bs (B.singleton 0)))
-	in buildKey b'
+        let l  = (keyLength `for` k) `div` 8
+            b' = B.take l (B.concat $ replicate l (B.append bs (B.singleton 0)))
+        in buildKey b'
 
 bIV :: BlockCipher k => k -> B.ByteString -> Either String (IV k)
 bIV k bs = Ser.decode bs
@@ -153,8 +153,8 @@ comparePadded k enc dec msg = unpadESP (dec k (enc k (padESPBlockSize k msg))) =
 
 prop_ECBEncDecID :: BlockCipher k => k -> B.ByteString -> B.ByteString -> Property
 prop_ECBEncDecID k kBS msg = goodKey k kBS ==>
-	let key = bKey k kBS
-	in comparePadded key ecb unEcb msg
+        let key = bKey k kBS
+        in comparePadded key ecb unEcb msg
 
 prop_BlockMode_EncDec_ID :: BlockCipher k =>
                             (k -> IV k -> B.ByteString -> (B.ByteString, IV k)) -> -- enc mode
@@ -165,7 +165,7 @@ prop_BlockMode_EncDec_ID :: BlockCipher k =>
                             B.ByteString ->  -- The message
                             Property
 prop_BlockMode_EncDec_ID enc dec k kBS ivBS msg = goodKey k kBS && isRight (bIV k ivBS) ==>
-	let key = bKey k kBS
+        let key = bKey k kBS
             Right iv = bIV k ivBS
             msg' = padESPBlockSize key msg
             (ct, iv2) = enc key iv msg'
@@ -198,14 +198,14 @@ prop_StrictLazyEq :: BlockCipher k =>
                      Property
 prop_StrictLazyEq enc' enc dec' dec k kBS ivBS msg = goodKey k kBS &&
                                                      isRight (bIV k ivBS) ==>
-	let key = bKey k kBS
-	    Right iv = bIV k ivBS
-	    msg' = takeBlockSize k msg
-	    ctStrict = enc' key iv (l2b msg')
-	    ctLazy   = enc  key iv msg'
-	    ptStrict = dec' key iv (l2b msg')
-	    ptLazy   = dec key iv msg'
-	in ctStrict == first l2b ctLazy && ptStrict == first l2b ptLazy
+        let key = bKey k kBS
+            Right iv = bIV k ivBS
+            msg' = takeBlockSize k msg
+            ctStrict = enc' key iv (l2b msg')
+            ctLazy   = enc  key iv msg'
+            ptStrict = dec' key iv (l2b msg')
+            ptLazy   = dec key iv msg'
+        in ctStrict == first l2b ctLazy && ptStrict == first l2b ptLazy
                                       
 
 prop_OFBStrictLazyEq = prop_StrictLazyEq ofb M.ofb unOfb M.unOfb
@@ -215,13 +215,13 @@ prop_CTRStrictLazyEq = prop_StrictLazyEq ctr (M.ctr incIV) unCtr (M.unCtr incIV)
 
 prop_ECBStrictLazyEq :: BlockCipher k => k -> B.ByteString -> L.ByteString -> Property
 prop_ECBStrictLazyEq k kBS msg = goodKey k kBS ==>
-	let key = bKey k kBS
-	    msg' = takeBlockSize k msg
-	    ctStrict = ecb key (l2b msg')
-	    ctLazy   = M.ecb  key msg'
-	    ptStrict = unEcb key (l2b msg')
-	    ptLazy   = M.unEcb key msg'
-	in ctStrict == l2b ctLazy && ptStrict == l2b ptLazy
+        let key = bKey k kBS
+            msg' = takeBlockSize k msg
+            ctStrict = ecb key (l2b msg')
+            ctLazy   = M.ecb  key msg'
+            ptStrict = unEcb key (l2b msg')
+            ptLazy   = M.unEcb key msg'
+        in ctStrict == l2b ctLazy && ptStrict == l2b ptLazy
 
 -- | Build test groups for encrypt/decrypt identity and 
 -- equality of operations on strict and lazy ByteStrings.
@@ -231,17 +231,17 @@ prop_ECBStrictLazyEq k kBS msg = goodKey k kBS ==>
 -- but more testing isn't exactly a bad thing.
 makeBlockCipherPropTests :: BlockCipher k => k -> [Test]
 makeBlockCipherPropTests k =
-	testGroup "Block Cipher tests (ident)"
-	[ testProperty "ECBEncDecID" (prop_ECBEncDecID k)
-	, testProperty "CBCEncDecID" (prop_CBCEncDecID k)
-	, testProperty "CFBEncDecID" (prop_CFBEncDecID k)
-	, testProperty "OFBEncDecID" (prop_OFBEncDecID k)
-	, testProperty "CTREncDecID" (prop_CTREncDecID k)] :
-	testGroup "Block Cipher tests (lazy/string bytestring equality)"
-	[ testProperty "ECBStringLazyEq" (prop_ECBStrictLazyEq k)
-	, testProperty "CBCStrictLazyEq" (prop_CBCStrictLazyEq k)
-	, testProperty "CFBStrictLazyEq" (prop_CFBStrictLazyEq k)
-	, testProperty "OFBStrictLazyEq" (prop_OFBStrictLazyEq k)
+        testGroup "Block Cipher tests (ident)"
+        [ testProperty "ECBEncDecID" (prop_ECBEncDecID k)
+        , testProperty "CBCEncDecID" (prop_CBCEncDecID k)
+        , testProperty "CFBEncDecID" (prop_CFBEncDecID k)
+        , testProperty "OFBEncDecID" (prop_OFBEncDecID k)
+        , testProperty "CTREncDecID" (prop_CTREncDecID k)] :
+        testGroup "Block Cipher tests (lazy/string bytestring equality)"
+        [ testProperty "ECBStringLazyEq" (prop_ECBStrictLazyEq k)
+        , testProperty "CBCStrictLazyEq" (prop_CBCStrictLazyEq k)
+        , testProperty "CFBStrictLazyEq" (prop_CFBStrictLazyEq k)
+        , testProperty "OFBStrictLazyEq" (prop_OFBStrictLazyEq k)
         , testProperty "CTRStrictLazyEq" (prop_CTRStrictLazyEq k)] :[]
 
 -- *Known Answer Tests
